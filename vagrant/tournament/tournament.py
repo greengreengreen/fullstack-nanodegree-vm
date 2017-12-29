08c113a9d95b75a692cf5d6877a352ac09a07e3a
GX <gxxuan71@gmail.com>
2017-12-28 21:47:22 -0600

    tournament_db

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -4,24 +4,37 @@
 #
 
 import psycopg2
-
+import bleach
+import numpy as np
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
-
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    db = connect()
+    c = db.cursor()
+    c.execute('delete from Matches;')
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    db = connect()
+    c = db.cursor()
+    c.execute('delete from Players;')
+    db.commit()
+    db.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
-
+    db = connect()
+    c = db.cursor()
+    c.execute("select count(*) from players;")
+    count = c.fetchone()[0]
+    db.close()
+    return count
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
@@ -32,7 +45,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-
+    db = connect()
+    c = db.cursor()
+    c.execute("insert into players (NAME) values (%s)", (bleach.clean(name),))
+    db.commit()
+    db.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -47,7 +64,27 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-
+    standings = []
+    db = connect()
+    c = db.cursor()
+    ids_query = 'select id from players;'
+    c.execute(ids_query)
+    ids = c.fetchall()   
+    for id_ in ids:
+        id_ = id_[0]
+        name = 'select name from players where id = %d;' %id_
+        wins = 'select count(*) from matches where winner = %d;' %id_
+        matches = 'select count(*) from matches where winner = %d or loser = %d;'%(id_, id_)
+        c.execute(name)
+        name = c.fetchone()[0]
+        c.execute(wins)
+        wins = c.fetchone()[0]
+        c.execute(matches)
+        matches = c.fetchone()[0]
+        standings.append((id_, name, wins, matches))
+    db.close()
+    return standings
+    
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -56,6 +93,11 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    db = connect()
+    c = db.cursor()
+    c.execute("insert into matches values (%d, %d)" % (winner,loser))  # Better, but ...
+    db.commit()
+    db.close()
  
  
 def swissPairings():
@@ -74,4 +116,17 @@ def swissPairings():
         name2: the second player's name
     """
 
+    pairings = []
+    standings = playerStandings()
+    dtype = [('id', int), ('name', 'S10'), ('wins', int), ('matches', int)]
+    values = standings
+    a = np.array(values, dtype=dtype)       # create a structured array
+    pairings_temp = np.sort(a, order='wins') 
+    for i in range(0,len(pairings_temp)/2):
+        id1 = pairings_temp[(2*i)][0]
+        name1 = pairings_temp[(2*i)][1]
+        id2 = pairings_temp[(2*i+1)][0]
+        name2 = pairings_temp[(2*i+1)][1]
+        pairings.append((id1, name1, id2, name2))
+    return pairings
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,16 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+CREATE TABLE Players(
+	ID SERIAL PRIMARY KEY,
+	NAME VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Matches(
+	WINNER INT NOT NULL,
+	LOSER INT NOT NULL,
+	PRIMARY KEY (WINNER, LOSER)
+);
+
 
 
